64fb813d26625ced4946d52e9482f2210357ed48
hossam.alissa <hossam.alissa@yahoo.com>
2021-02-04 02:52:39 +0200

    ADD layoutBuilder in HS

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_budget_ui/data/data.dart';
+import 'package:flutter_budget_ui/helpers/color_helper.dart';
 import 'package:flutter_budget_ui/models/category_model.dart';
 import 'package:flutter_budget_ui/models/expense_model.dart';
 import 'package:flutter_budget_ui/widgets/bar_chart.dart';
@@ -13,6 +14,7 @@ class _HomeScreenState extends State<HomeScreen> {
   _buildCategory(Category category, double totalAmountSpent) {
     return Container(
       margin: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+      padding: EdgeInsets.all(20.0),
       height: 100.0,
       width: double.infinity,
       decoration: BoxDecoration(
@@ -38,10 +40,47 @@ class _HomeScreenState extends State<HomeScreen> {
               ),
               Text(
                 '\$${(category.maxAmount - totalAmountSpent).toStringAsFixed(2)} / \$${(category.maxAmount).toStringAsFixed(2)}',
-                style: TextStyle(fontSize: 18.0,fontWeight: FontWeight.w600),
+                style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.w600),
               ),
             ],
-          )
+          ),
+          SizedBox(height: 10.0),
+          LayoutBuilder(
+            builder: (BuildContext context,BoxConstraints constraints) {
+              // print(constraints.maxHeight);
+              // print(constraints.minHeight);
+
+              final double maxBarWidth = constraints.maxHeight;
+              final double percent = (category.maxAmount - totalAmountSpent) / category.maxAmount;
+              double barWidth = percent * 300;
+              print(percent);
+              print(barWidth);
+              print(maxBarWidth);
+              if(barWidth < 0){
+                barWidth = 0;
+              }
+              return Stack(
+                children: [
+                  Container(
+                    height: 20.0,
+                    width: 300,
+                    decoration: BoxDecoration(
+                      color: Colors.grey[200],
+                      borderRadius: BorderRadius.circular(15.0),
+                    ),
+                  ),
+                  Container(
+                    height: 20.0,
+                    width: barWidth.toDouble(),
+                    decoration: BoxDecoration(
+                      color: getColor(context , percent),
+                      borderRadius: BorderRadius.circular(15.0),
+                    ),
+                  ),
+                ],
+              );
+            },
+          ),
         ],
       ),
     );
